6055452a8fb4064b0f02c6a89c781562fd048787
Gopikrishna Arepalli <a.gopikrishna01@gmail.com>
2024-11-05 12:31:21 +0530

    Update 000001_sum_of_two.py

--- 000001_sum_of_two.py
+++ 000001_sum_of_two.py
@@ -1,1 +1,21 @@
+# Method - 1
 
+def twoSum(nums, target):
+    for x in range(len(nums)):
+        for y in range(len(nums)):
+            if x != y and nums[x] + nums[y] == target:
+                return [x, y]
+
+print(twoSum([3,2,4], 6))
+
+
+
+# Method - 2
+
+def twoSum(nums, target):
+    for x in range(len(nums)):
+        for y in range(x+ 1, len(nums)):
+            if x != y and nums[x] + nums[y] == target:
+                return [x, y]
+
+print(twoSum([3,2,4], 6))
